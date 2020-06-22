68f736a957861ded67d7c14739764dd808de7b0b
Clint Brodar <clintbrodar@gmail.com>
2020-06-22 12:39:50 -0700

    Analog clock

--- src/components/clock/index.js
+++ src/components/clock/index.js
@@ -1,9 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import './style.scss';
 
 function Clock() {
-    const [displayTime, setDisplayTime] = useState("");
+    const [displayTime, setDisplayTime] = useState({
+        hours: '',
+        minutes: '',
+        seconds: '',
+        amOrPm: ''
+    });
+    // const [displayTime, setDisplayTime] = useState("");
 
     setInterval(() => {
         const currentSecs = new Date() - new Date(0);
@@ -11,13 +17,47 @@ function Clock() {
         let minutes = moment(new Date(currentSecs)).format('mm');
         let seconds = moment(new Date(currentSecs)).format('ss');
         let amOrPm = moment(new Date(currentSecs)).format('A');
-        setDisplayTime(`${hours}:${minutes}:${seconds} ${amOrPm}`);
+        setDisplayTime({
+            hours, minutes, seconds, amOrPm
+        });
+        const deg = 6;
+        const hr = document.querySelector('#hr');
+        const min = document.querySelector('#mn');
+        const sec = document.querySelector('#sc');
+        let day = new Date();
+        let hh = day.getHours() * 30;
+        let mm = day.getMinutes() * deg;
+        let ss = day.getSeconds() * deg;
+        hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+        min.style.transform = `rotateZ(${mm}deg)`;
+        sec.style.transform = `rotateZ(${ss}deg)`;
     }, 1000);
+
+
 
     return (
         <div className="clock-section">
-            <div className="clock">
-                {displayTime}
+            <div className="clock-bg">
+                <div className="clock">
+                    <div className="hour">
+                        <div className="hr" id="hr">
+
+                        </div>
+                    </div>
+                    <div className="min">
+                        <div className="mn" id="mn">
+
+                        </div>
+                    </div>
+                    <div className="sec">
+                        <div className="sc" id="sc">
+
+                        </div>
+                    </div>
+                    <div className="digital">
+                        {`${displayTime.hours}:${displayTime.minutes}:${displayTime.seconds} ${displayTime.amOrPm}`}
+                    </div>
+                </div>
             </div>
         </div>
     )
